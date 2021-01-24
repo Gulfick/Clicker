f7aab6710a93752123f6f85e69da9b4c8f10fb25
Kirill Stepushkin <astep4@rambler.ru>
2021-01-24 17:59:39 +0300

    Lesson 2: First scripts

---
+++ Assets/Script/EarnMoneyButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarnMoneyButton : MonoBehaviour
+{
+    [SerializeField] private GameManager _manager;
+    
+    public void ButtonClick()
+    {
+        _manager.AddMoneyForClick();
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _textMoney;
+    private long _money;
+
+    private int _moneyForClick = 1;
+
+
+    private void AddMoney(long cash)
+    {
+        _money += cash;
+        _textMoney.text = _money.ToString();
+    }
+
+    public void AddMoneyForClick()
+    {
+        AddMoney(_moneyForClick);
+    }
+}
